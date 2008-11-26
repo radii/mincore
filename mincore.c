ae1b75f8e64701f9660a8487ca16c79f6601165d
Andy Isaacson <adi@hexapodia.org>
2008-11-26 13:24:17 -0800

    Significant code restructuring, type cleanups, and munmap() after finishing with a file to avoid VMA exhaustion.

--- mincore.c
+++ mincore.c
@@ -19,8 +19,8 @@ void die(char *fmt, ...)
     exit(1);
 }
 
-void basic_info(char *fname, char *vec, int npg);
-void summary_info(char *fname, char *vec, int npg);
+void basic_info(char *fname, unsigned char *vec, int npg);
+void summary_info(char *fname, unsigned char *vec, int npg);
 
 static int o_verbose = 0;
 static int o_fullname = 0;
@@ -30,13 +30,77 @@ void usage(char *progname)
     die("Usage: %s [-f] [-v] file1 [file2 ...]\n", progname);
 }
 
+static int pagesize;
+
+int mincore_one(char *filename)
+{
+    int fd, npg;
+    struct stat st;
+    char *outname;
+    unsigned char *vec;
+    void *map = 0;
+    int retval = 1;
+
+    if((fd = open(filename, O_RDONLY)) == -1) {
+	fprintf(stderr, "%s: %s\n", filename, strerror(errno));
+	retval = 0;
+	goto out;
+    }
+
+    if(fstat(fd, &st) == -1) {
+	fprintf(stderr, "fstat: %s\n", strerror(errno));
+	retval = 0;
+	goto out_close;
+    }
+
+    npg = st.st_size / pagesize + ((st.st_size % pagesize) != 0);
+    vec = malloc(npg);
+    if(!vec) {
+	fprintf(stderr, "malloc(%d): %s\n", npg, strerror(errno));
+	retval = 0;
+	goto out_close;
+    }
+    if(st.st_size) {
+	map = mmap(0, st.st_size, PROT_READ, MAP_SHARED, fd, 0);
+	if(map == MAP_FAILED) {
+	    fprintf(stderr, "mmap(%s): %s\n", filename, strerror(errno));
+	    retval = 0;
+	    goto out_free;
+	}
+	if(mincore(map, st.st_size, vec) == -1) {
+	    fprintf(stderr, "mincore: %s\n", strerror(errno));
+	    retval = 0;
+	    goto out_munmap;
+	}
+    }
+    if(o_fullname)
+	outname = filename;
+    else {
+	outname = strrchr(filename, '/');
+	outname = outname ? outname + 1 : filename;
+    }
+    if(o_verbose)
+	summary_info(outname, vec, npg);
+    else
+	basic_info(outname, vec, npg);
+
+out_munmap:
+    if(map && munmap(map, st.st_size) == -1)
+	fprintf(stderr, "munmap(%p, %ld): %s\n", map, st.st_size,
+		strerror(errno));
+out_free:
+    free(vec);
+out_close:
+    close(fd);
+out:
+    return retval;
+}
+
 int main(int argc, char **argv)
 {
-    int c, fd, npg;
-    struct stat st;
-    int pagesize = getpagesize();
-    char *vec, *outname;
-    void *map;
+    int c;
+
+    pagesize = getpagesize();
 
     while((c = getopt(argc, argv, "fhv")) != -1) {
 	switch(c) {
@@ -49,38 +113,12 @@ int main(int argc, char **argv)
     }
 
     for(; optind < argc; optind++) {
-	if((fd = open(argv[optind], O_RDONLY)) == -1)
-	    die("%s: %s\n", argv[optind], strerror(errno));
-
-	if(fstat(fd, &st) == -1)
-	    die("fstat: %s\n", strerror(errno));
-
-	npg = st.st_size / pagesize + ((st.st_size % pagesize) != 0);
-	vec = malloc(npg);
-	if(!vec)
-	    die("malloc(%d): %s\n", npg, strerror(errno));
-	if(st.st_size) {
-	    map = mmap(0, st.st_size, PROT_READ, MAP_SHARED, fd, 0);
-	    if(map == MAP_FAILED)
-		die("mmap(%s): %s\n", argv[optind], strerror(errno));
-	    if(mincore(map, st.st_size, vec) == -1)
-		die("mincore: %s\n", strerror(errno));
-	}
-	if(o_fullname)
-	    outname = argv[optind];
-	else {
-	    outname = strrchr(argv[optind], '/');
-	    outname = outname ? outname + 1 : argv[optind];
-	}
-	if(o_verbose)
-	    summary_info(outname, vec, npg);
-	else
-	    basic_info(outname, vec, npg);
+	mincore_one(argv[optind]);
     }
     return 0;
 }
 
-int count_present(char *vec, int n, int *runs)
+int count_present(unsigned char *vec, int n, int *runs)
 {
     int i, ret, last = -1;
 
@@ -96,7 +134,7 @@ int count_present(char *vec, int n, int *runs)
     return ret;
 }
 
-void basic_info(char *fname, char *vec, int npg)
+void basic_info(char *fname, unsigned char *vec, int npg)
 {
     int nrun, npresent;
 
@@ -118,7 +156,7 @@ void basic_info(char *fname, char *vec, int npg)
  * Uses Bresenham's algorithm to determine which pages map to a given
  * character in the output.
  */
-void summary_info(char *fname, char *vec, int npg)
+void summary_info(char *fname, unsigned char *vec, int npg)
 {
     int i, width = 60, pgc, err, de, slope;
     const long long SCALE = 1024;
